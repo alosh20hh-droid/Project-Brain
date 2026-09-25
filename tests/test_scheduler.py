8a8249db369893f5bf3be613a089987b09aeb6ff
alosh20hh-droid <alosh.20hh@gmail.com>
2026-09-26 02:06:00 +0300

    Test exclusive scheduler claims

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -6,3 +6,9 @@ def test_locked_task_is_skipped():
  s=Scheduler(g,locks)
  items=s.claim_ready("w",{"a":["repo"],"b":["db"]},limit=2)
  assert [x.task_id for x in items]==["b"]
+
+
+def test_same_task_cannot_be_claimed_twice_without_release():
+ g=TaskGraph();g.add(TaskNode("a"));s=Scheduler(g,LockManager())
+ assert [x.task_id for x in s.claim_ready("w1",{},1)]==["a"]
+ assert s.claim_ready("w2",{},1)==[]
