4d711b7168081f4caac6b5aada586ca03544915a
alosh20hh-droid <alosh.20hh@gmail.com>
2026-09-26 11:46:22 +0300

    Export compatible model provider

--- src/project_brain/model/__init__.py
+++ src/project_brain/model/__init__.py
@@ -1,3 +1,5 @@
 from .provider import ModelProvider,ModelProviderError
 from .types import ModelMessage,ModelResponse,ModelToolCall
 from .registry import ModelRegistry
+
+from .openai_compatible import OpenAICompatibleProvider
