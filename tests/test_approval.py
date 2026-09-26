f8ff9765f9635b7c43be438602f7b09602f316c3
alosh20hh-droid <alosh.20hh@gmail.com>
2026-09-26 12:40:35 +0300

    Fix strict execution contract regression tests

--- tests/test_approval.py
+++ tests/test_approval.py
@@ -3,7 +3,7 @@ from project_brain.approval import ApprovalRequired, require_owner_approval
 from project_brain.contracts import ExecutionRequest, RiskLevel
 
 def test_sensitive_action_requires_approval():
-    req = ExecutionRequest(task_id="pay", goal="pay", risk=RiskLevel.SENSITIVE)
+    req = ExecutionRequest(task_id="pay", goal="pay", operation_id="op-pay", risk=RiskLevel.SENSITIVE)
     with pytest.raises(ApprovalRequired):
         require_owner_approval(req)
 
@@ -13,5 +13,5 @@ def test_low_risk_does_not_require_approval():
 
 
 def test_boolean_true_cannot_bypass_sensitive_approval():
- req=ExecutionRequest(task_id="pay2",goal="pay",risk=RiskLevel.SENSITIVE)
+ req=ExecutionRequest(task_id="pay2",goal="pay",operation_id="op-pay2",risk=RiskLevel.SENSITIVE)
  with pytest.raises(ApprovalRequired):require_owner_approval(req,approved=True)
