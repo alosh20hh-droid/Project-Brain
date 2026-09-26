4aa68b8d5303f4cb3196235e52aa077c4d12ea21
alosh20hh-droid <alosh.20hh@gmail.com>
2026-09-26 12:36:31 +0300

    Align safety tests with strict execution contract

--- tests/test_guardrails.py
+++ tests/test_guardrails.py
@@ -2,5 +2,5 @@ from project_brain.contracts import ExecutionRequest,RiskLevel
 from project_brain.guardrails import Guardrails
 
 def test_irreversible_blocked_before_gateway():
-    r=Guardrails().check(ExecutionRequest(task_id="x",goal="delete",risk=RiskLevel.IRREVERSIBLE))
+    r=Guardrails().check(ExecutionRequest(task_id="x",goal="delete",operation_id="op-x",risk=RiskLevel.IRREVERSIBLE))
     assert not r.allowed
