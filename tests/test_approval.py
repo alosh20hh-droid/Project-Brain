7fa5edde43dd7dd8150c315f90cc3e4f59a221e9
alosh20hh-droid <alosh.20hh@gmail.com>
2026-09-26 11:00:19 +0300

    Test legacy boolean cannot bypass approval gateway

--- tests/test_approval.py
+++ tests/test_approval.py
@@ -10,3 +10,8 @@ def test_sensitive_action_requires_approval():
 def test_low_risk_does_not_require_approval():
     req = ExecutionRequest(task_id="read", goal="read")
     require_owner_approval(req)
+
+
+def test_boolean_true_cannot_bypass_sensitive_approval():
+ req=ExecutionRequest(task_id="pay2",goal="pay",risk=RiskLevel.SENSITIVE)
+ with pytest.raises(ApprovalRequired):require_owner_approval(req,approved=True)
